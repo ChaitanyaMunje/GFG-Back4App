83fcea6517a347d65f3fcb22c47c1bc79df63765
Chaitanya Munje <chaitanyamunje@gmail.com>
2021-04-20 16:30:45 +0530

    PayPal SDK Integration in Android.

--- app/src/main/java/com/gtappdevelopers/gfgparse/MainActivity.java
+++ app/src/main/java/com/gtappdevelopers/gfgparse/MainActivity.java
@@ -1,259 +1,116 @@
 package com.gtappdevelopers.gfgparse;
 
-import android.Manifest;
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.SearchView;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuItemCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.DexterError;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.PermissionRequestErrorListener;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
-    //creating variables for our array list, recycler view progress bar and adapter.
-    private ArrayList<ContactsModal> contactsModalArrayList;
-    private RecyclerView contactRV;
-    private ContactRVAdapter contactRVAdapter;
-    private ProgressBar loadingPB;
+
+    public static final String clientKey = "Enter Client ID";
+    public static final int PAYPAL_REQUEST_CODE = 123;
+    //Paypal Configuration Object
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            // Start with mock environment.  When ready, switch to sandbox (ENVIRONMENT_SANDBOX)
+            // or live (ENVIRONMENT_PRODUCTION)
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            //on below line we are passing a client id.
+            .clientId(clientKey);
+    private EditText amountEdt;
+    private TextView paymentTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //on below line we are initializing our variables.
-        contactsModalArrayList = new ArrayList<>();
-        contactRV = findViewById(R.id.idRVContacts);
-        FloatingActionButton addNewContactFAB = findViewById(R.id.idFABadd);
-        loadingPB = findViewById(R.id.idPBLoading);
-        //calling method to prepare our recycler view.
-        prepareContactRV();
-        //calling a method to request permissions.
-        requestPermissions();
-        //adding on click listener for our fab.
-        addNewContactFAB.setOnClickListener(new View.OnClickListener() {
+        amountEdt = findViewById(R.id.idEdtAmount);
+        //creating a variable for button, edit text and status tv.
+        Button makePaymentBtn = findViewById(R.id.idBtnPay);
+        paymentTV = findViewById(R.id.idTVStatus);
+        //on below line adding click listner to our make payment button.
+        makePaymentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //opening a new activity on below line.
-                Intent i = new Intent(MainActivity.this, CreateNewContactActivity.class);
-                startActivity(i);
+                //calling a method to get payment.
+                getPayment();
             }
         });
+
+    }
+
+    private void getPayment() {
+        //Getting the amount from editText
+        String amount = amountEdt.getText().toString();
+        //Creating a paypal payment on below line.
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(amount)), "USD", "Course Fees",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        //Creating Paypal Payment activity intent
+        Intent intent = new Intent(this, PaymentActivity.class);
+
+        //putting the paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        //Puting paypal payment to the intent
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
+        //Starting the intent activity for result
+        //the request code will be used on the method onActivityResult
+        startActivityForResult(intent, PAYPAL_REQUEST_CODE);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //in this on create options menu we are calling a menu inflater and inflating our menu file.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search_menu, menu);
-        //on below line we are getting our menu item as search view item
-        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
-        //on below line we are creating a variable for our search view.
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
-        //on below line we are setting on query text listner for our search view.
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //on query submit we are clearing the focus forour search view.
-                searchView.clearFocus();
-                return false;
-            }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //If the result is from paypal
+        if (requestCode == PAYPAL_REQUEST_CODE) {
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //on changing the text in our search view we are calling a filter method to filter our array list.
-                filter(newText.toLowerCase());
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }
+            //If the result is OK i.e. user has not canceled the payment
+            if (resultCode == Activity.RESULT_OK) {
+                //Getting the payment confirmation
+                PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
 
-    private void filter(String text) {
-        //in this method we are filtering our array list.
-        //on below line we are creating a new filtered array list.
-        ArrayList<ContactsModal> filteredlist = new ArrayList<>();
-        //on below line we are running a loop for checking if the item is present in array list.
-        for (ContactsModal item : contactsModalArrayList) {
-            if (item.getUserName().toLowerCase().contains(text.toLowerCase())) {
-                //on below line we are adding item to our filtered array list.
-                filteredlist.add(item);
-            }
-        }
-        //on below line we are checking if the filtered list is empty or not.
-        if (filteredlist.isEmpty()) {
-            Toast.makeText(this, "No Contact Found", Toast.LENGTH_SHORT).show();
-        } else {
-            //passing this filtered list to our adapter with filter list method.
-            contactRVAdapter.filterList(filteredlist);
-        }
-    }
-
-    private void prepareContactRV() {
-        //in thi method we are preparing our recycler view with adapter.
-        contactRVAdapter = new ContactRVAdapter(this, contactsModalArrayList);
-        //on below line we are setting layout mnager.
-        contactRV.setLayoutManager(new LinearLayoutManager(this));
-        //on below line we are setting adapter to our recycler view.
-        contactRV.setAdapter(contactRVAdapter);
-    }
-
-    private void requestPermissions() {
-        // below line is use to request
-        // permission in the current activity.
-        Dexter.withActivity(this)
-                // below line is use to request the number of
-                // permissions which are required in our app.
-                .withPermissions(Manifest.permission.READ_CONTACTS,
-                        // below is the list of permissions
-                        Manifest.permission.CALL_PHONE,
-                        Manifest.permission.SEND_SMS, Manifest.permission.WRITE_CONTACTS)
-                // after adding permissions we are
-                // calling an with listener method.
-                .withListener(new MultiplePermissionsListener() {
-                    @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
-                        // this method is called when all permissions are granted
-                        if (multiplePermissionsReport.areAllPermissionsGranted()) {
-                            // do you work now
-                            getContacts();
-                            Toast.makeText(MainActivity.this, "All the permissions are granted..", Toast.LENGTH_SHORT).show();
-                        }
-                        // check for permanent denial of any permission
-                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
-                            // permission is denied permanently,
-                            // we will show user a dialog message.
-                            showSettingsDialog();
-                        }
+                //if confirmation is not null
+                if (confirm != null) {
+                    try {
+                        //Getting the payment details
+                        String paymentDetails = confirm.toJSONObject().toString(4);
+                        //on below line we are extracting json response and displaying it in a text view.
+                        JSONObject payObj = new JSONObject(paymentDetails);
+                        String payID = payObj.getJSONObject("response").getString("id");
+                        String state = payObj.getJSONObject("response").getString("state");
+                        paymentTV.setText("Payment " + state + "\n with payment id is " + payID);
+                    } catch (JSONException e) {
+                        //handling json exception on below line
+                        Log.e("Error", "an extremely unlikely failure occurred: ", e);
                     }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
-                        // this method is called when user grants some
-                        // permission and denies some of them.
-                        permissionToken.continuePermissionRequest();
-                    }
-                }).withErrorListener(new PermissionRequestErrorListener() {
-            // this method is use to handle error
-            // in runtime permissions
-            @Override
-            public void onError(DexterError error) {
-                // we are displaying a toast message for error message.
-                Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
-            }
-        })
-                // below line is use to run the permissions
-                // on same thread and to check the permissions
-                .onSameThread().check();
-    }
-
-    // below is the shoe setting dialog
-    // method which is use to display a
-    // dialogue message.
-    private void showSettingsDialog() {
-        // we are displaying an alert dialog for permissions
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-
-        // below line is the title
-        // for our alert dialog.
-        builder.setTitle("Need Permissions");
-
-        // below line is our message for our dialog
-        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
-        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // this method is called on click on positive
-                // button and on clicking shit button we
-                // are redirecting our user from our app to the
-                // settings page of our app.
-                dialog.cancel();
-                // below is the intent from which we
-                // are redirecting our user.
-                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                Uri uri = Uri.fromParts("package", getPackageName(), null);
-                intent.setData(uri);
-                startActivityForResult(intent, 101);
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // this method is called when
-                // user click on negative button.
-                dialog.cancel();
-            }
-        });
-        // below line is used
-        // to display our dialog
-        builder.show();
-    }
-
-    private void getContacts() {
-        //this method is use to read contact from users device.
-        //on below line we are creating a string variables for our contact id and display name.
-        String contactId = "";
-        String displayName = "";
-        //on below line we are calling our content resolver for getting contacts
-        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
-        //on blow line we are checking the count for our cursor.
-        if (cursor.getCount() > 0) {
-            //if the count is greater thatn 0 then we are running a loop to move our cursor to next.
-            while (cursor.moveToNext()) {
-                //on below line we are getting the phone number.
-                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
-                if (hasPhoneNumber > 0) {
-                    //we are checking if the has phine number is >0
-                    //on below line we are getting our contact id and user name for that contact
-                    contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                    displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                    //on below line we are calling a content resolver and making a query
-                    Cursor phoneCursor = getContentResolver().query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            new String[]{contactId},
-                            null);
-                    //on below line we are moving our cursor to next position.
-                    if (phoneCursor.moveToNext()) {
-                        //on below line we are getting the phone number for our users and then adding the name along with phone number in array list.
-                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        contactsModalArrayList.add(new ContactsModal(displayName, phoneNumber));
-                    }
-                    //on below line we are closing our phone cursor.
-                    phoneCursor.close();
                 }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                //on below line we are checking the payment status.
+                Log.i("paymentExample", "The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                //on below line when the invalid paypal config is submitted.
+                Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
             }
         }
-        //on below line we are closing our cursor.
-        cursor.close();
-        //on below line we are hiding our progress bar and notifying our adapter class.
-        loadingPB.setVisibility(View.GONE);
-        contactRVAdapter.notifyDataSetChanged();
     }
-
 }
